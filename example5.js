8550eb9332196d53e1cf40d855f3ac69b55f3703
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-10-01 16:59:32 -0500

    test

--- example5.js
+++ example5.js
@@ -4,7 +4,7 @@ console.log(birds)
 birds.forEach( function(bird, index) {
     //repeat one for each object
     //fun will be called for each object in the array
-    console.log(index, birds.toUpperCase())
+    console.log(index, birds.toUpperCase());
 })
 
 for (let x = 0; x < birds.length ; x++ ) {
